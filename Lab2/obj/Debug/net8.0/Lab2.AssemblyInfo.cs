c21123b6e4fc728fe0f88763bbf863870139088c
Victork1327 <S01320851@acad.tri-c.edu>
2025-04-25 18:02:11 +0000

    lab 6 finished

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a11a17ac27f7f6e87383ab30b366971b7a61b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7096ec0998c512cf1d70cd2780249e0e3432d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Car
+{
+  public string? model;//added the "?" to get rid of the warning and make it nullable 
+  public string? color;
+  
+  public int year;
+  public void Start(){
+    Console.WriteLine("The car is starting."); //creating a start method to later be used
+}
+  public void Drive(int x){
+Console.WriteLine($"The car drove {x} miles.");
+  }//making a method with a parameter so that we can interchange the variable 
+  public string GetDescription(){
+    return $"{year} {color} {model}";//using a method to have one clear description line that could be stored on its own
+  }
+ public void Repaint(string newColor){
+    color = newColor;
+    Console.WriteLine($"The car has been repainted to {color}");
+ }
+  
+  public void Display()
+  {
+    Console.WriteLine("Car Details:");
+    Console.WriteLine("Model: " + model);
+    Console.WriteLine("Color: " + color);
+    Console.WriteLine("Year: " + year);
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    // Creating a Car object and assigning values to its properties
+    Car myCar = new Car();
+    myCar.model = "Civic";
+    myCar.color = "Black";
+    myCar.year = 2020;
+
+    myCar.Display(); // Calling the Display method
+    myCar.Start();//implementing a start method 
+    myCar.Drive(50);
+    string desc = myCar.GetDescription();//storing the method as a voriable that can be used later on
+   Console.WriteLine(desc);
+   myCar.Repaint("Red");//using the method to choose a color red now 
+   Console.WriteLine(myCar.color);//showing how the color field has been updated
+
+  }
+}
